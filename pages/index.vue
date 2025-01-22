f380607205e64beda198df9265fc1b6b40604836
Paymushkin <paymushkin_a@bk.ru>
2025-01-22 13:39:05 +0300

    upd

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <HeroSection />
+  <RecommendationsSection />
+  <ExchangeInfoSection />
+</template>
+
+<script setup>
+import HeroSection from '../src/components/mainPage/HeroSection.vue'
+import RecommendationsSection from '../src/components/mainPage/RecommendationsSection.vue'
+import ExchangeInfoSection from '../src/components/mainPage/ExchangeInfoSection.vue'
+</script> 
