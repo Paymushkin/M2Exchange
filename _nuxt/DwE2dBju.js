41cb275757f8cff570901effa253ba0c16469d92
Paymushkin <paymushkin_a@bk.ru>
2025-01-23 01:48:08 +0300

    Updates

---
+++ _nuxt/DwE2dBju.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./DlAUqK2U.js";import{c as o,a as c,o as s}from"./BGJ8ScWA.js";const n={},r={class:"flex items-center justify-center p-10"};function a(f,e){return s(),o("div",r,e[0]||(e[0]=[c("h1",{class:"text-4xl font-bold"},"Account Page",-1)]))}const i=t(n,[["render",a]]);export{i as default};
