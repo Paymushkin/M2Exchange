6dc655f1a9d7aea863807c50083c78bf3cc5687e
Paymushkin <paymushkin_a@bk.ru>
2025-01-22 13:38:55 +0300

    upd

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="min-h-screen flex flex-col">
+    <TheHeader />
+    <main class="flex flex-grow flex-col">
+      <slot />
+    </main>
+    <TheFooter />
+  </div>
+</template>
+
+<script setup>
+import TheHeader from '~/src/components/TheHeader.vue'
+import TheFooter from '~/src/components/TheFooter.vue'
+</script> 
