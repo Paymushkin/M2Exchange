6f1c0cdc06ece59b6278839eb9bac42834219dbd
Paymushkin <paymushkin_a@bk.ru>
2025-01-29 23:45:03 +0300

    Updates

--- assets/rA-PPi91.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,c as s,r}from"./CExKfF1L.js";const n={},c={class:"bg-dark text-sm text-white rounded px-4 h-[50px]"};function a(t,_){return o(),s("button",c,[r(t.$slots,"default")])}const l=e(n,[["render",a]]);export{l as T};
