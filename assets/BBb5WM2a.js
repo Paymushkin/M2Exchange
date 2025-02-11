8551666fef64a2bb1cba44bd9f5e9ea40fbebb35
Paymushkin <paymushkin_a@bk.ru>
2025-02-11 14:35:33 +0300

    Updates

---
+++ assets/BBb5WM2a.js
@@ -0,0 +1,1 @@
+import"./CgwX3g7S.js";const r=""+new URL("avatar.Bg-8EY3a.png",import.meta.url).href;export{r as o};
--- assets/BwpwFT3J.js
+++
@@ -1,1 +0,0 @@
-import"./BwzQTlu_.js";const r=""+new URL("avatar.Bg-8EY3a.png",import.meta.url).href;export{r as o};
