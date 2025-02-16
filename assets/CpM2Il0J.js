94b6b4bfdff5f719e7195279a770934e30c1fb7c
Paymushkin <paymushkin_a@bk.ru>
2025-02-16 13:14:17 +0300

    Updates

---
+++ assets/CpM2Il0J.js
@@ -0,0 +1,1 @@
+import{_ as t,c,a as s,o}from"./BzY_WlIR.js";const n={},a={class:"flex items-center justify-center p-10"};function r(f,e){return o(),c("div",a,e[0]||(e[0]=[s("h1",{class:"text-4xl font-bold"},"Account Page",-1)]))}const _=t(n,[["render",r]]);export{_ as default};
